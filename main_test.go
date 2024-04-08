ff82faae5a244a94e0afafc28655dcab601c8843
Miek Gieben <miek@miek.nl>
2024-04-08 16:09:31 +0200

    more tests
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- main_test.go
+++ main_test.go
@@ -29,3 +29,16 @@ $ORIGIN example.org.
 		t.Fatalf("failed to properly reformat\n%s\n", out.String())
 	}
 }
+
+func TestFormatCommentStart(t *testing.T) {
+	const mess = `; example.nl,v 1.00 2015/03/19 14:31:47 root Exp
+$ORIGIN example.nl.
+`
+	out := &bytes.Buffer{}
+	Reformat([]byte(mess), nil, out)
+	if out.String() != `; example.nl,v 1.00 2015/03/19 14:31:47 root Exp
+$ORIGIN example.nl.
+` {
+		t.Fatalf("failed to properly reformat\n%s\n", out.String())
+	}
+}
