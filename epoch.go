ea69d3266b32da993d6a45b8f49483b9188ea7a3
Miek Gieben <miek@miek.nl>
2024-04-13 10:07:08 +0200

    add date after serial
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- epoch.go
+++ epoch.go
@@ -22,19 +22,20 @@ func SerialToHuman(s []byte) string {
 
 	// epoch?
 	i, err := strconv.ParseInt(string(s), 10, 64)
-	if err == nil {
-		// check the time interval
-		now := time.Now()
-		t := time.Unix(i, 0)
-		if now.Sub(t) > Year15 {
-			return dateToHuman(s)
-		}
-		if t.Sub(now) > Year5 {
-			return dateToHuman(s)
-		}
-		return "  " + t.UTC().Format(time.RFC1123)
+	if err != nil {
+		return "  " + dateToHuman(s)
 	}
-	return "  " + dateToHuman(s)
+
+	// check the time interval
+	now := time.Now()
+	t := time.Unix(i, 0)
+	if now.Sub(t) > Year15 {
+		return "  " + dateToHuman(s)
+	}
+	if t.Sub(now) > Year5 {
+		return "  " + dateToHuman(s)
+	}
+	return "  " + t.UTC().Format(time.RFC1123)
 }
 
 func dateToHuman(s []byte) string {
@@ -46,8 +47,6 @@ func dateToHuman(s []byte) string {
 	mon, _ := strconv.ParseInt(string(s[4:6]), 10, 64)
 	day, _ := strconv.ParseInt(string(s[6:8]), 10, 64)
 	hour, _ := strconv.ParseInt(string(s[8:10]), 10, 64)
-
-	println(year, mon, day, hour)
 
 	t := time.Date(int(year), time.Month(mon), int(day), int(hour), 0, 0, 0, time.UTC)
 	return t.Format(time.RFC1123)
--- epoch_test.go
+++ epoch_test.go
@@ -8,6 +8,8 @@ func TestSerialToHuman(t *testing.T) {
 		out string
 	}{
 		{"1282630063", "Tue, 24 Aug 2010 06:07:43 UTC"},
+		{"2024041300", "Sat, 13 Apr 2024 00:00:00 UTC"},
+		{"2024041301", "Sat, 13 Apr 2024 01:00:00 UTC"},
 	}
 	for i, ts := range tests {
 		if x := SerialToHuman([]byte(ts.in)); x != "  "+ts.out {
